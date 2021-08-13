f30710a12c6b701a34c8683acb41a625002f5519
alexandru <boncut.alex10@gmail.com>
2021-08-12 22:40:36 -0500

    Add real looking data using Faker

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,22 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+20.times {Author.create!(name:Faker::Book.unique.author, about:Faker::Quote.famous_last_words)}
+
+10.times {Genre.create!(name:Faker::Book.unique.genre)}
+
+40.times {Book.create!(title:Faker::Book.unique.title, description:Faker::Lorem.paragraph, author_id:rand(1..20), genre_id:rand(1..10))}
+
+5.times {
+  user = User.create!(username:Faker::Internet.unique.username, email:Faker::Internet.unique.email, img_url:"")
+         FavoriteBook.create!(user:user, book_id:rand(1..40))
+         FinishedBook.create!(user:user, book_id:rand(1..40))
+         ReadingNowBook.create!(user:user, book_id:rand(1..40))
+         BucketListBook.create!(user:user, book_id:rand(1..40))
+         Library.create!(user:user, book_id:rand(1..40))
+}
+
+80.times {
+  opinion = Opinion.create!(content:Faker::Lorem.sentence, user_id:rand(1..5), book_id:rand(1..40))
+            Like.create!(user_id:rand(1..5), opinion:opinion)
+}
+
+
+
