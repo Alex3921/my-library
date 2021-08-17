c15f50c1864eb36cadf3b7b6d49783af866767fc
alexandru <boncut.alex10@gmail.com>
2021-08-16 22:43:20 -0500

    Add helper methods
    The helper methods are:
    * current_user
    * logged_in?
    * authenticate_user?

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  def current_user
+    @current_user ||= User.find_by_id(session[:user_id])
+  end
+
+  def logged_in?
+    !!current_user
+  end
+
+  def authenticate_user!
+    redirect_to login_path if !logged_in?
+  end
 end
