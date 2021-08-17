e4f21bac5be0fe3ee4c7bd9dc439dd0d3e4ea5b9
alexandru <boncut.alex10@gmail.com>
2021-08-16 22:48:40 -0500

    Add validations for user model
    I also changed the the models relationship with a 'library' model.
    - a user has_one :library != has_many :libraries

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
+  has_secure_password
+  validates_presence_of :email, :username
+  validates_uniqueness_of :email, :username
+
   has_many :favorite_books
   has_many :books_favorite, through: :favorite_books, source: :book
   has_many :finished_books
@@ -7,7 +11,7 @@ class User < ApplicationRecord
   has_many :books_bucket_list, through: :bucket_list_books, source: :book
   has_many :reading_now_books
   has_many :books_reading_now, through: :reading_now_books, source: :book
-  has_many :libraries
-  has_many :library_books, through: :libraries, source: :book
+  has_many :library
+  has_many :library_books, through: :library, source: :book
   has_many :opinions
 end
