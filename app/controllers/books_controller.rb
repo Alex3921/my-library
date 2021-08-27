75a345f02156a80cb2e7040c02088801603920f7
alexandru <boncut.alex10@gmail.com>
2021-08-26 23:55:39 -0500

    Add show, index methods
    Include helper and callback.

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,11 +1,23 @@
 class BooksController < ApplicationController
+  before_action :set_current_user
+  include BooksHelper
 
   def index
-    @books = Book.all
+    if params[:user_id]
+      method_from_url = request.original_url.split('/').last
+      @books = @current_user.send(method_from_url)
+    else
+      @books = Book.all
+    end
   end
 
   def show
     @book = Book.find(params[:id])
+  end
+
+  private
+  def book_params
+    params.require(:book).permit(opinion: [:content])
   end
 
 end
