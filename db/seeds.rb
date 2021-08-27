c46461c278a8330d765b4e2256501c9f53341b44
alexandru <boncut.alex10@gmail.com>
2021-08-27 00:28:38 -0500

    Updated

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,22 +1,17 @@
 20.times {Author.create!(name:Faker::Book.unique.author, about:Faker::Quote.famous_last_words)}
 
-10.times {Genre.create!(name:Faker::Book.unique.genre)}
+80.times {Book.create!(title:Faker::Book.unique.title, description:Faker::Lorem.paragraph_by_chars(number: 256, supplemental: false), author_id:rand(1..20))}
 
-40.times {Book.create!(title:Faker::Book.unique.title, description:Faker::Lorem.paragraph, author_id:rand(1..20), genre_id:rand(1..10))}
-
-5.times {
-  user = User.create!(username:Faker::Internet.unique.username, email:Faker::Internet.unique.email, img_url:"")
-         FavoriteBook.create!(user:user, book_id:rand(1..40))
-         FinishedBook.create!(user:user, book_id:rand(1..40))
-         ReadingNowBook.create!(user:user, book_id:rand(1..40))
-         BucketListBook.create!(user:user, book_id:rand(1..40))
-         Library.create!(user:user, book_id:rand(1..40))
+20.times {
+  user = User.create!(username:Faker::Internet.unique.username, email:Faker::Internet.unique.email, img_url:"", password:"asd123", password_confirmation:"asd123")
+         FavoriteBook.create!(user:user, book_id:rand(1..80))
+         FinishedBook.create!(user:user, book_id:rand(1..80))
+         ReadingNowBook.create!(user:user, book_id:rand(1..80))
+         BucketListBook.create!(user:user, book_id:rand(1..80))
 }
 
 80.times {
   opinion = Opinion.create!(content:Faker::Lorem.sentence, user_id:rand(1..5), book_id:rand(1..40))
             Like.create!(user_id:rand(1..5), opinion:opinion)
 }
-
-
 
