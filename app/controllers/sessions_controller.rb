530fa2947aeaa5e55f66f4142b6a4228f385f28e
alexandru <boncut.alex10@gmail.com>
2021-08-26 23:59:29 -0500

    Add omniauth login. Update create action

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,24 +4,37 @@ class SessionsController < ApplicationController
   end
   
   def create
-    @user = User.find_by(username: params[:user][:username]).try(:authenticate, params[:user][:password])
+    user = User.find_by(username: params[:user][:username]).try(:authenticate, params[:user][:password])
     
-    if @user
-      session[:user_id] = @user.id
-      redirect_to about_path
+    if user
+      session[:user_id] = user.id
+      redirect_to user_path(user),  notice: 'Logged in successfully'
     else
-      flash[:message] = "Invalid credentials. Please try again!"
+      flash[:alert] = "Invalid credentials. Please try again!"
       redirect_to login_path
     end
   end
 
   def omniauth
+    user = User.from_omniauth(oauth_response)
 
+    if user.valid?
+      session[:user_id] = user.id
+      redirect_to user_path(user),  notice: 'Logged in successfully'
+    else
+      flash[:alert] = "Invalid credentials. Please try again!"
+      redirect_to login_path
+    end
   end
 
   def destroy
     reset_session
     redirect_to login_path
+  end
+
+  private
+  def oauth_response
+    request.env['omniauth.auth']
   end
 
 end
