e18cc538230b24bb8e2e86a51fd33c6e074156dc
alexandru <boncut.alex10@gmail.com>
2021-08-27 00:00:58 -0500

    Update method names

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,13 +1,13 @@
 module ApplicationHelper
-  def current_user
+  def set_current_user
     @current_user ||= User.find_by_id(session[:user_id])
   end
 
   def logged_in?
-    !!current_user
+    !!set_current_user
   end
 
-  def authenticate_user!
-    redirect_to login_path if !logged_in?
+  def require_user_logged_in!
+    redirect_to login_path, alert: 'You must be signed in!' if !logged_in?
   end
 end
