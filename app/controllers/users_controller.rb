a9c4230b4752b4e50320115d44f50a6ae47837c8
alexandru <boncut.alex10@gmail.com>
2021-08-29 21:45:50 -0500

    Read description
    * Include UsersHelper
    * Add callback to make sure a user can't access another's profile
    * Add flash messages
    * Add edit, update, destroy actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,30 +1,54 @@
 class UsersController < ApplicationController
-  before_action :require_user_logged_in!, only: [:show]
+  include UsersHelper
+  before_action :require_user_logged_in!, only: [:show, :edit, :update]
+  before_action :user_is_allowed?, only: [:show, :edit, :update]
+
+  def show
+    @current_user = User.find(session[:user_id])
+  end
 
   def new
     @user = User.new
   end
 
   def create
-    byebug
     @user = User.new(user_params)
 
     if @user.save
       session[:user_id] = @user.id
-      redirect_to library_path(@user.library), notice: 'Account created successfully!'
+      redirect_to user_path(@user), notice: 'Account created successfully!'
     else
-      render :new
+      render :new, flash.now[:alert] = "There was an error. Please try again!"
     end
   end
 
-  def show
+  def edit
     @user = User.find(session[:user_id])
+  end
+
+  def update
+    byebug
+    @user = User.find(session[:user_id])
+
+    if @user.update(user_params)
+      redirect_to user_path(@user), notice: 'Account updated successfully!'
+    else
+      render :edit, flash.now[:alert] = "There was an error. Please try again!"
+    end
+  end
+
+  def destroy
+    @user = User.find(session[:user_id])
+
+    @user.destroy
+    reset_session
+    redirect_to login_path, notice: "User was successfully destroyed!"
   end
 
 private
 
   def user_params
-    params.require(:user).permit(:username, :email, :password, :password_confirmation)
+    params.require(:user).permit(:username, :email, :img_url, :password, :password_confirmation)
   end
 
 end
