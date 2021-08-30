266b7abeb131428f9376d6ca02cbe75ebd8e06e6
alexandru <boncut.alex10@gmail.com>
2021-08-29 21:40:50 -0500

    Add flash messages

--- app/controllers/favorite_books_controller.rb
+++ app/controllers/favorite_books_controller.rb
@@ -2,14 +2,15 @@ class FavoriteBooksController < ApplicationController
   before_action :require_user_logged_in!
 
   def create
-    user_fav = @current_user.favorite_books.find_by(book_id: params[:book_id])
+    @book = Book.find(params[:book_id])
+    user_fav = @current_user.favorite_books.find_by(book: @book)
     if user_fav
       FavoriteBook.delete(user_fav)
-      redirect_to book_path(params[:book_id])
+      redirect_to book_path(@book), alert: "#{@book.title} has been removed from favorites."
     else
-      @current_user.favorite_books.build(book_id: params[:book_id])
+      @current_user.favorite_books.build(book: @book)
       @current_user.save
-      redirect_to book_path(params[:book_id])
+      redirect_to book_path(@book), notice: "#{@book.title} has been added to favorites."
     end
   end
 
