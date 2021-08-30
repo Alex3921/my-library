b95caff261686cfb387fc487b42fa1cd7fc8afca
alexandru <boncut.alex10@gmail.com>
2021-08-29 21:41:28 -0500

    Add flash messages

--- app/controllers/reading_now_books_controller.rb
+++ app/controllers/reading_now_books_controller.rb
@@ -2,14 +2,15 @@ class ReadingNowBooksController < ApplicationController
   before_action :require_user_logged_in!
 
   def create
-    user_reading = @current_user.reading_now_books.find_by(book_id: params[:book_id])
+    @book = Book.find(params[:book_id])
+    user_reading = @current_user.reading_now_books.find_by(book: @book)
     if user_reading
       ReadingNowBook.delete(user_reading)
-      redirect_to book_path(params[:book_id])
+      redirect_to book_path(@book), alert: "#{@book.title} has been removed from the list."
     else
-      @current_user.reading_now_books.build(book_id: params[:book_id])
+      @current_user.reading_now_books.build(book: @book)
       @current_user.save
-      redirect_to book_path(params[:book_id])
+      redirect_to book_path(@book), notice: "You're currently reading #{@book.title}."
     end
   end
 end
