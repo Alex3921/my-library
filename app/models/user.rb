e86c1c84c11e28b4e82e16febb7a4a761b71b69c
alexandru <boncut.alex10@gmail.com>
2021-08-29 21:48:32 -0500

    Add destroy dependency

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,15 +1,16 @@
 class User < ApplicationRecord
   has_secure_password
-  validates_presence_of :email, :username
+  validates_presence_of :email, :username, on: :create
   validates_uniqueness_of :email, :username, message: 'An account associated with %{value} already exists'
 
-  has_many :favorite_books
+  has_one_attached :img_url
+  has_many :favorite_books, dependent: :destroy
   has_many :favorites, through: :favorite_books, source: :book
-  has_many :finished_books
+  has_many :finished_books, dependent: :destroy
   has_many :finished, through: :finished_books, source: :book
-  has_many :bucket_list_books
+  has_many :bucket_list_books, dependent: :destroy
   has_many :bucket_list, through: :bucket_list_books, source: :book
-  has_many :reading_now_books
+  has_many :reading_now_books, dependent: :destroy
   has_many :reading_now, through: :reading_now_books, source: :book
 
   def self.from_omniauth(response)
