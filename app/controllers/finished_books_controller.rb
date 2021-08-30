2adf8c1f51e62e27728d0fd7023f45c927ecd467
alexandru <boncut.alex10@gmail.com>
2021-08-29 21:41:01 -0500

    Add flash messages

--- app/controllers/finished_books_controller.rb
+++ app/controllers/finished_books_controller.rb
@@ -2,14 +2,15 @@ class FinishedBooksController < ApplicationController
   before_action :require_user_logged_in!
 
   def create
-    user_fin = @current_user.finished_books.find_by(book_id: params[:book_id])
+    @book = Book.find(params[:book_id])
+    user_fin = @current_user.finished_books.find_by(book: @book)
     if user_fin
       FinishedBook.delete(user_fin)
-      redirect_to book_path(params[:book_id])
+      redirect_to book_path(params[:book_id]), alert: "#{@book.title} has been removed from your finished books."
     else
-      @current_user.finished_books.build(book_id: params[:book_id])
+      @current_user.finished_books.build(book: @book)
       @current_user.save
-      redirect_to book_path(params[:book_id])
+      redirect_to book_path(@book), notice: "#{@book.title} has been added to your finished books."
     end
   end
 end
