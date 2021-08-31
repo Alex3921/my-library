bafa7be8d65e3d883dc4d34b13ca1d67234c0b59
alexandru <boncut.alex10@gmail.com>
2021-08-30 22:20:29 -0500

    Add cover/img_url to existing data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,9 @@
 20.times {Author.create!(name:Faker::Book.unique.author, about:Faker::Quote.famous_last_words)}
 
-80.times {Book.create!(title:Faker::Book.unique.title, description:Faker::Lorem.paragraph_by_chars(number: 256, supplemental: false), author_id:rand(1..20))}
+80.times {Book.create!(title:Faker::Book.unique.title, cover:"/app/assets/images/coming-soon.jpg", description:Faker::Lorem.paragraph_by_chars(number: 256, supplemental: false), author_id:rand(1..20))}
 
 20.times {
-  user = User.create!(username:Faker::Internet.unique.username, email:Faker::Internet.unique.email, img_url:"", password:"asd123", password_confirmation:"asd123")
+  user = User.create!(username:Faker::Internet.unique.username, email:Faker::Internet.unique.email, img_url:"/app/assets/images/coming-soon.jpg", password:"asd123", password_confirmation:"asd123")
          FavoriteBook.create!(user:user, book_id:rand(1..80))
          FinishedBook.create!(user:user, book_id:rand(1..80))
          ReadingNowBook.create!(user:user, book_id:rand(1..80))
