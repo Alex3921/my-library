231d5051b7175f57c64cdf04de486b0f2c7979eb
alexandru <boncut.alex10@gmail.com>
2021-09-01 16:13:55 -0500

    Add 'like_icon' and 'likes_count' methods

--- app/helpers/likes_helper.rb
+++ app/helpers/likes_helper.rb
@@ -1,2 +1,17 @@
 module LikesHelper
+  def like_icon(opinion)
+    if logged_in? 
+      if opinion.likes.find_by(user: @current_user)
+        (content_tag(:i, "", class:"fas fa-heart m-2") + "Dislike")
+      else
+        (content_tag(:i, "", class:"far fa-heart m-2") + "Like")
+      end
+    else
+      "far fa-heart"
+    end
+  end
+
+  def likes_count(opinion)
+    "#{pluralize(Like.where(opinion: opinion).count, "like")}"
+  end
 end
