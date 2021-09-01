90b39403f335986f0c9bf06d11ad35432669a340
alexandru <boncut.alex10@gmail.com>
2021-09-01 16:12:38 -0500

    Add 'create' action

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,2 +1,18 @@
 class LikesController < ApplicationController
+  before_action :require_user_logged_in!
+  include LikesHelper
+
+  def create
+    @book = Book.find(params[:book_id])
+    @opinion = Opinion.find(params[:opinion_id])
+    like = @opinion.likes.find_by(user: @current_user)
+    if !!like
+      Like.delete(like)
+      redirect_to book_path(@book)
+    else
+      @opinion.likes.build(user: @current_user )
+      @opinion.save
+      redirect_to book_path(@book)
+    end
+  end
 end
