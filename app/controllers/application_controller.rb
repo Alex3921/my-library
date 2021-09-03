d5e2fcd9bd458c8cb5c11c671932705116e02fa1
alexandru <boncut.alex10@gmail.com>
2021-09-03 10:09:11 -0500

    Add 'route_not_found' method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,8 @@ class ApplicationController < ActionController::Base
   include ApplicationHelper
   protect_from_forgery with: :exception
   rescue_from ActiveRecord::RecordNotFound, with: :record_not_found!
+
+  def route_not_found
+    render file: Rails.public_path.join('404.html'), status: :not_found, layout: false
+  end
 end
